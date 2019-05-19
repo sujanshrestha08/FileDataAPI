6318784f9e22cb45dfd41dda5ccb04e36f8ea0d2
Softwarica College Lab <softwarica@softwarica.com>
2019-05-19 11:55:35 +0545

    Added API classes

--- app/src/main/java/com/e/filedataapi/MainActivity.java
+++ app/src/main/java/com/e/filedataapi/MainActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import heroesapi.HeroesAPI;
 import model.Heroes;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
+import url.Url;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,13 +43,33 @@ public class MainActivity extends AppCompatActivity {
             String name = etName.getText().toString();
             String desc = etDescription.getText().toString();
 
-            Heroes heroes = name Heroes(name, desc);
+            Heroes heroes = new Heroes(name, desc);
 
-            Retrofit retrofit = ne
+            final Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(Url.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
 
             HeroesAPI heroesAPI = retrofit.create(HeroesAPI.class);
 
-            Call<Void> heroesAPI.addHero(heroes);
+            Call<Void> heroesCall = heroesAPI.addHero(heroes);
+
+            heroesCall.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Code " + response.code(), Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    Toast.makeText(MainActivity.this, "Successfully Added", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+
+                }
+            });
 
     }
 
